404bb1e702f9682005a1f207df1dfa21a63e4639
root <root@ip-172-31-20-79.us-east-2.compute.internal>
2024-11-01 01:07:05 +0000

    terra

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-2"
+}
+resource "aws_instance" "amazon_linx" {
+  count                  = 1
+  ami                    = "ami-09da212cf18033880"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_http_and_ssh.id]
+  key_name               = "Mykey"
+  tags = {
+    name = "aws_instance"
+

0754fd2cebc278c4c83b740337c57b38f39ec627
root <root@ip-172-31-20-79.us-east-2.compute.internal>
2024-11-01 01:34:22 +0000

    file32

--- main.tf
+++ main.tf
@@ -5,7 +5,6 @@ resource "aws_instance" "amazon_linx" {
   count                  = 1
   ami                    = "ami-09da212cf18033880"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.allow_http_and_ssh.id]
   key_name               = "Mykey"
   tags = {
     name = "aws_instance"
